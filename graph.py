f39712e82e24d29a7a9fb91e39f5523565468b8f
Khaled Adad <khaledadad@lewisu.edu>
2020-01-20 23:37:04 -0600

    Completed Graphs Intro

--- graph.py
+++ graph.py
@@ -18,22 +18,29 @@ class Graph:
         self.graph_dict[from_vertex.value].add_edge(to_vertex.value, weight)
 
         if not self.directed:
-            self.graph_dict[to_vertex.value].to_vertex.add_edge(from_vertex.value, weight)
+            self.graph_dict[to_vertex.value].add_edge(from_vertex.value, weight)
 
     def find_path(self, start_vertex, end_vertex):
         start = [start_vertex]
+        # Use a dictionary to track which
+        # vertices we've already visited
+        seen = {}
         while len(start) > 0:
             current_vertex = start.pop(0)
-            # current_vertex is end_vertex
-            if current_vertex == end_vertex:
-                # a path exists!
-                return True
-            # current_vertex is not end_vertex
-            vertex = self.graph_dict[current_vertex]
-            # add vertices connected to 
-            next_vertices = vertex.get_edges()
-            # current_vertex onto the list
-            start.extend(next_vertices)
-            # to keep searching for a path
+            # Update the `seen` variable
+            # now that we've visited current_vertex
+            seen[current_vertex] = True
             print("Visiting " + current_vertex)
+            if current_vertex == end_vertex:
+                return True
+            else:
+                vertex = self.graph_dict[current_vertex]
+                next_vertices = vertex.get_edges()
+                
+                # Filter next_vertices so it only
+                # includes vertices NOT IN seen
+                next_vertices = [vertex for vertex in next_vertices if vertex not in seen]
+                #next_vertices = [vertex for vertex in next_vertices ???????]
+                start.extend(next_vertices)
+        
         return False
