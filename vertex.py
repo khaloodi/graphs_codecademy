82ca04e4855afe429a01a0b954091b0b5fbc9f3f
Khaled Adad <khaledadad@lewisu.edu>
2020-01-20 21:32:04 -0600

    Creating edges

--- vertex.py
+++ vertex.py
@@ -3,6 +3,15 @@ class Vertex:
         self.value = value 
         self.edges = {}
 
+    def add_edge(self, vertex):
+        """
+        Takes in a Vertex and adds an edge
+        Ex: vertex.add_edge(vertex2) creates an edge
+        b/w vertex one and vertex2
+        """
+        print('Adding edge to ' + vertex.value)
+        self.edges[vertex.value] = True
+
     def get_edges(self):
         """
         Returns a list of the keys of the edges dictionary
@@ -10,5 +19,10 @@ class Vertex:
         return list(self.edges.keys())
 
 
-station = Vertex("Cronk")
-print(station.get_edges())
+lake = Vertex("Lake")
+print(lake.get_edges())
+
+chicago = Vertex("Chicago")
+lake.add_edge(chicago)
+
+print(lake.get_edges(), chicago.get_edges())
