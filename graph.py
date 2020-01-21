189850a3c25c201067a862ad5e57e4deb6e3f08d
Khaled Adad <khaledadad@lewisu.edu>
2020-01-20 23:17:25 -0600

    Fixing bugs

--- graph.py
+++ graph.py
@@ -15,10 +15,10 @@ class Graph:
         self.graph_dict[vertex.value] = vertex 
 
     def add_edge(self, from_vertex, to_vertex, weight=0):
-        self.graph_dict[from_vertex.value] = from_vertex.add_edge(to_vertex.value, weight)
+        self.graph_dict[from_vertex.value].add_edge(to_vertex.value, weight)
 
         if not self.directed:
-            self.graph_dict[to_vertex.value] = to_vertex.add_edge(from_vertex.value, weight)
+            self.graph_dict[to_vertex.value].to_vertex.add_edge(from_vertex.value, weight)
 
     def find_path(self, start_vertex, end_vertex):
         start = [start_vertex]
