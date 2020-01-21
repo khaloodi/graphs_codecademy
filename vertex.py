ba75b3f85fe5e66fb3b93c8e8285e9403b84eca6
Khaled Adad <khaledadad@lewisu.edu>
2020-01-20 21:07:51 -0600

    Adding to vertext

--- vertex.py
+++ vertex.py
@@ -1,3 +1,14 @@
-class Vertex(self, value):
-    self.value = value 
-    self.edges = {}
+class Vertex:
+    def __init__(self, value):    
+        self.value = value 
+        self.edges = {}
+
+    def get_edges(self):
+        """
+        Returns a list of the keys of the edges dictionary
+        """
+        return list(self.edges.keys())
+
+
+station = Vertex("Cronk")
+print(station.get_edges())
