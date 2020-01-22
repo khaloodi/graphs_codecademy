16887e71a111615ae1ae1bf0206ad629c8cccc84
Khaled Adad <khaledadad@lewisu.edu>
2020-01-21 22:12:45 -0600

    Finished DFS

--- graph_search/dfs_recursive.py
+++ graph_search/dfs_recursive.py
@@ -27,3 +27,31 @@ def dfs(graph, current_vertex, target_value, visited=None):
     visited.append(current_vertex)
     # build out base case, if the current vertex is the same 
     # as the value we are looking for, then, done searching, return our path (the visited list)
+    if current_vertex == target_value:
+        return visited 
+
+    # add recursive case 
+    # loop through each neighbor of the current vertex in the graph:
+    for neighbor in graph[current_vertex]:
+        # if neighbor has not been visited:
+        if neighbor not in visited:
+        # recursively call dfs on neighbor
+            path = dfs(graph, neighbor, target_value, visited)
+            # if a path exists:
+            if path:
+            # return the path
+                return path 
+    return None 
+
+
+the_most_dangerous_graph = {
+    'lava': set(['sharks', 'piranhas']),
+    'sharks': set(['lava', 'bees', 'lasers']),
+    'piranhas': set(['lava', 'crocodiles']),
+    'bees': set(['sharks']),
+    'lasers': set(['sharks', 'crocodiles']),
+    'crocodiles': set(['piranhas', 'lasers'])
+}
+
+# Call dfs() below and print the result:
+print(dfs(the_most_dangerous_graph, 'crocodiles', 'bees'))
