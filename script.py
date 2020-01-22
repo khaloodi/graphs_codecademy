b74d1111ebe52497cfa2455eb3a647b6716d9045
Khaled Adad <khaledadad@lewisu.edu>
2020-01-21 20:26:23 -0600

    Added search/traversal files

---
+++ script.py
@@ -0,0 +1,8 @@
+"""
+There are three main traversal orders that you’ll come across for graph traversal:
+
+Preorder, in which each vertex is added to the “visited” list and added to the output list BEFORE getting added to the stack
+Postorder, in which each vertex is added to the “visited” list and added to the output list AFTER it is popped off the stack
+Reverse Post-Order (also known as Topological Sort), which returns an output list that is exactly the reverse of the post-order list
+"""
+
